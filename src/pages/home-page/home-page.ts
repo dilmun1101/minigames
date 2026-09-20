f1f08e49f3a2a7e6d3d2905a5465830a200b543c
dilmun1101 <dilmun94@gmail.com>
2026-09-21 00:44:52 +0500

    feat: change text

--- src/pages/home-page/home-page.ts
+++ src/pages/home-page/home-page.ts
@@ -7,7 +7,7 @@ class HomePage extends BaseComponent {
     const header = new Header();
     const text = new BaseComponent({
       tag: 'p',
-      text: 'Home page',
+      text: 'Could you please check my work on Wednesday? ༼ つ ◕_◕ ༽つ',
     });
 
     super(
