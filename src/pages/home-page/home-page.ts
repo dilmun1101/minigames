19798d37b7d9d4ce0d7ac7b2ee6149fb8a7cce1a
dilmun1101 <dilmun94@gmail.com>
2026-09-20 16:29:10 +0500

    feat: change HomePage using BaseComponent

--- src/pages/home-page/home-page.ts
+++ src/pages/home-page/home-page.ts
@@ -1,12 +1,28 @@
-import Header from '../../widgets/header/header';
+import Header from '@/widgets/header/header';
+import BaseComponent from '@/shared/lib/base-component/base-component';
+import styles from './home-page.module.scss';
 
-export class HomePage {
-  render() {
-    const page = document.createElement('div');
+class HomePage extends BaseComponent {
+  constructor() {
     const header = new Header();
-    const p = document.createElement('p');
-    p.innerHTML = `Home page`;
-    page.append(header.node, p);
-    return page;
+    const text = new BaseComponent({
+      tag: 'p',
+      text: 'Home page',
+    });
+
+    super(
+      {
+        tag: 'div',
+        className: styles.wrapper,
+      },
+      header,
+      text
+    );
+  }
+
+  public render(): HTMLElement {
+    return this.node;
   }
 }
+
+export default HomePage;
